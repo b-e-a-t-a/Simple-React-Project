f50a83c6d5a7d12ad93878e4b8244a54bcef836b
Beata Karolczak <karolczak.beata@gmail.com>
2018-03-15 23:08:02 +0100

    Add script in index

---
+++ README.md
@@ -0,0 +1,2 @@
+**Very simple Profile Page in React based on Tutorial: https://medium.com/learning-new-stuff/building-your-first-react-js-app-d53b0c98dc
+**
---
+++ index.js
@@ -0,0 +1,10 @@
+/*var App = React.createClass({
+	render: function() {
+		return (
+			<div>Hello World!</div>
+		);
+	}
+});
+
+ReactDOM.render(<App />, document.getElementById('content'));
+*/
